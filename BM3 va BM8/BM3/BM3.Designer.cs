560a8f52ccdcba984cd6af5ce241d174ae164b3e
DarkDipper <72306288+DarkDipper@users.noreply.github.com>
2020-10-04 12:20:14 +0700

    Add files via upload
    Them BM3 va BM8 va Trang Chu

---
+++ BM3 va BM8/BM3/BM3.Designer.cs
@@ -0,0 +1,318 @@
+﻿namespace BM3
+{
+    partial class BM3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.tri_gia = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.nguoi_nhan = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.nam_xb = new System.Windows.Forms.TextBox();
+            this.Tac_gia = new System.Windows.Forms.TextBox();
+            this.nha_xb = new System.Windows.Forms.TextBox();
+            this.ten_sach = new System.Windows.Forms.TextBox();
+            this.ngay_nhan = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.errorProvider2 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.the_loai = new System.Windows.Forms.ComboBox();
+            this.dong = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoEllipsis = true;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(29, 69);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(122, 23);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Tên Sách:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(25, 126);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(130, 23);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Năm xuất bản:";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(25, 184);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(100, 23);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Thể loại";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label5
+            // 
+            this.label5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label5.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(29, 230);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(83, 34);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Tác giả:";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(29, 288);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(142, 23);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Nhà xuất bản:";
+            // 
+            // label6
+            // 
+            this.label6.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label6.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(370, 69);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(78, 23);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Trị giá:";
+            // 
+            // tri_gia
+            // 
+            this.tri_gia.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.tri_gia.Location = new System.Drawing.Point(543, 70);
+            this.tri_gia.Name = "tri_gia";
+            this.tri_gia.Size = new System.Drawing.Size(122, 22);
+            this.tri_gia.TabIndex = 5;
+            this.tri_gia.Validating += new System.ComponentModel.CancelEventHandler(this.tri_gia_Validating);
+            // 
+            // label7
+            // 
+            this.label7.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label7.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(370, 125);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(143, 23);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Ngày nhập:";
+            // 
+            // nguoi_nhan
+            // 
+            this.nguoi_nhan.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.nguoi_nhan.Location = new System.Drawing.Point(543, 184);
+            this.nguoi_nhan.Name = "nguoi_nhan";
+            this.nguoi_nhan.Size = new System.Drawing.Size(180, 22);
+            this.nguoi_nhan.TabIndex = 7;
+            this.nguoi_nhan.Validating += new System.ComponentModel.CancelEventHandler(this.nguoi_nhan_Validating);
+            // 
+            // label8
+            // 
+            this.label8.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label8.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(370, 182);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(167, 23);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "Người tiếp nhận:";
+            // 
+            // nam_xb
+            // 
+            this.nam_xb.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.nam_xb.Location = new System.Drawing.Point(159, 125);
+            this.nam_xb.Name = "nam_xb";
+            this.nam_xb.Size = new System.Drawing.Size(180, 22);
+            this.nam_xb.TabIndex = 1;
+            this.nam_xb.Validating += new System.ComponentModel.CancelEventHandler(this.nam_xb_Validating);
+            // 
+            // Tac_gia
+            // 
+            this.Tac_gia.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.Tac_gia.Location = new System.Drawing.Point(159, 237);
+            this.Tac_gia.Name = "Tac_gia";
+            this.Tac_gia.Size = new System.Drawing.Size(180, 22);
+            this.Tac_gia.TabIndex = 3;
+            this.Tac_gia.Validating += new System.ComponentModel.CancelEventHandler(this.Tac_gia_Validating);
+            // 
+            // nha_xb
+            // 
+            this.nha_xb.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.nha_xb.Location = new System.Drawing.Point(159, 288);
+            this.nha_xb.Name = "nha_xb";
+            this.nha_xb.Size = new System.Drawing.Size(180, 22);
+            this.nha_xb.TabIndex = 4;
+            this.nha_xb.Validating += new System.ComponentModel.CancelEventHandler(this.nha_xb_Validating);
+            // 
+            // ten_sach
+            // 
+            this.ten_sach.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.ten_sach.Location = new System.Drawing.Point(159, 69);
+            this.ten_sach.Name = "ten_sach";
+            this.ten_sach.Size = new System.Drawing.Size(180, 22);
+            this.ten_sach.TabIndex = 0;
+            this.ten_sach.Validating += new System.ComponentModel.CancelEventHandler(this.ten_sach_Validating);
+            // 
+            // ngay_nhan
+            // 
+            this.ngay_nhan.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.ngay_nhan.Location = new System.Drawing.Point(543, 127);
+            this.ngay_nhan.Name = "ngay_nhan";
+            this.ngay_nhan.Size = new System.Drawing.Size(180, 22);
+            this.ngay_nhan.TabIndex = 6;
+            this.ngay_nhan.Validating += new System.ComponentModel.CancelEventHandler(this.ngay_nhan_Validating);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.AutoSize = true;
+            this.button1.Location = new System.Drawing.Point(319, 350);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(129, 38);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Xác Nhận";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.BlinkRate = 0;
+            this.errorProvider1.BlinkStyle = System.Windows.Forms.ErrorBlinkStyle.NeverBlink;
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // errorProvider2
+            // 
+            this.errorProvider2.BlinkRate = 0;
+            this.errorProvider2.BlinkStyle = System.Windows.Forms.ErrorBlinkStyle.NeverBlink;
+            this.errorProvider2.ContainerControl = this;
+            // 
+            // the_loai
+            // 
+            this.the_loai.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.the_loai.FormattingEnabled = true;
+            this.the_loai.Items.AddRange(new object[] {
+            "Hành động và phiêu lưu",
+            "Cổ điển",
+            "Sách hình",
+            "Trinh thám",
+            "Viễn tưởng",
+            "Lịch sử",
+            "Kinh dị",
+            "Văn học",
+            "Lãng mạn",
+            "Khoa học"});
+            this.the_loai.Location = new System.Drawing.Point(159, 181);
+            this.the_loai.Name = "the_loai";
+            this.the_loai.Size = new System.Drawing.Size(180, 24);
+            this.the_loai.TabIndex = 2;
+            this.the_loai.Validating += new System.ComponentModel.CancelEventHandler(this.the_loai_Validating);
+            // 
+            // dong
+            // 
+            this.dong.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.dong.Enabled = false;
+            this.dong.Location = new System.Drawing.Point(664, 70);
+            this.dong.Name = "dong";
+            this.dong.Size = new System.Drawing.Size(59, 22);
+            this.dong.TabIndex = 18;
+            this.dong.Text = "   đồng";
+            // 
+            // BM3
+            // 
+            this.AcceptButton = this.button1;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(757, 479);
+            this.Controls.Add(this.dong);
+            this.Controls.Add(this.the_loai);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.ngay_nhan);
+            this.Controls.Add(this.ten_sach);
+            this.Controls.Add(this.nha_xb);
+            this.Controls.Add(this.Tac_gia);
+            this.Controls.Add(this.nam_xb);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.nguoi_nhan);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.tri_gia);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Name = "BM3";
+            this.Text = "Thông Tin Sách";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.BM3_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        internal System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox tri_gia;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox nguoi_nhan;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox nam_xb;
+        private System.Windows.Forms.TextBox Tac_gia;
+        private System.Windows.Forms.TextBox nha_xb;
+        private System.Windows.Forms.TextBox ten_sach;
+        private System.Windows.Forms.TextBox ngay_nhan;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.ErrorProvider errorProvider2;
+        private System.Windows.Forms.ComboBox the_loai;
+        private System.Windows.Forms.TextBox dong;
+    }
+}
+
---
+++ BM3 va BM8/BM3/BM3.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using System.Globalization;
+using System.Diagnostics.SymbolStore;
+
+namespace BM3
+{
+    public partial class BM3 : Form
+    {
+        public BM3()
+        {
+            InitializeComponent();
+            AutoValidate = AutoValidate.EnableAllowFocusChange;
+        }
+        //string[] type = { "Hành động và phiêu lưu","Cổ điển","Sách hình","Trinh thám","Viễn tưởng","Lịch sử","Kinh dị","Văn học","Lãng mạn","Khoa học" };
+        string tensach,theloai,nguoinhan,tacgia;
+        int namxb;
+        decimal trigia;
+        private void nam_xb_Validating(object sender, CancelEventArgs e)
+        {
+            if (Int32.TryParse(nam_xb.Text, out int x))
+            {
+                if ( DateTime.Now.Year - x <= 8&&DateTime.Now.Year>=x)
+                {
+                    e.Cancel = false;
+                    errorProvider1.SetError(nam_xb, null);
+                }
+                else
+                {
+                    e.Cancel= true;
+                    errorProvider1.SetError(nam_xb, "Yêu cầu sách xuất bản trong 8 năm đổi lại");
+                }
+            }
+            else
+            {
+                e.Cancel = true;
+                errorProvider1.SetError(nam_xb, "Vui lòng nhập năm");
+            }
+        }
+        private void Tac_gia_Validating(object sender, CancelEventArgs e)
+        {
+            if (!Only_letter(Tac_gia.Text)||Tac_gia.Text.Length==0)
+            {
+                e.Cancel = true;
+                errorProvider1.SetError(Tac_gia, "Vui lòng nhập đúng yêu cầu");
+            }
+            else
+            {
+                e.Cancel = false;
+                errorProvider1.SetError(Tac_gia, null);
+            }
+        }
+        private void nha_xb_Validating(object sender, CancelEventArgs e)
+        {
+            if (!Only_letter(nha_xb.Text)||nha_xb.Text.Length==0)
+            {
+                e.Cancel = true;
+                errorProvider1.SetError(nha_xb, "Vui lòng nhập đúng yêu cầu");
+            }
+            else
+            {
+                e.Cancel = false;
+                errorProvider1.SetError(nha_xb, null);
+            }
+        }
+        private void the_loai_Validating(object sender, CancelEventArgs e)
+        {
+            if (Only_letter(the_loai.Text)&&the_loai.Text.Length!=0)
+            { 
+                e.Cancel = false;
+                errorProvider1.SetError(the_loai, null);
+                
+            }
+            else
+            {
+                e.Cancel = true;
+                errorProvider1.SetError(the_loai, "Vui lòng chọn thể loại");
+            }
+        }
+        private void ten_sach_Validating(object sender, CancelEventArgs e)
+        {
+            if (ten_sach.Text.Length == 0)
+            {
+                
+                e.Cancel = true;
+                errorProvider1.SetError(ten_sach, "Vui lòng nhập tên sách");
+            }
+            else
+            {
+                e.Cancel = false;
+                errorProvider1.SetError(ten_sach, null);
+            }
+        }
+
+        private void tri_gia_Validating(object sender, CancelEventArgs e)
+        {
+            string[] part = tri_gia.Text.Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+            if (part.Length != 2)
+            {
+                e.Cancel = true;
+                errorProvider1.SetError(dong, "Vui lòng nhập đúng yêu cầu");
+            }
+            else
+            {
+                if (part[1] != "đồng")
+                {
+                    e.Cancel = true;
+                    errorProvider1.SetError(dong,"Đơn vị tiền tệ là đồng");
+                }
+                else if(!decimal.TryParse(part[0],out decimal x))
+                {
+                    e.Cancel = true;
+                    errorProvider1.SetError(dong, "Không định dạng được giá trị");
+                }
+                else
+                {
+                    trigia = x;
+                    e.Cancel = false;
+                    errorProvider1.SetError(tri_gia, null);
+                }
+            }
+        }
+        public bool Only_letter(string x)
+        {
+            char[] DS = { '\'','\\',',','.','/','<','>',';',':','\"','{','[','}',']',
+                          '~','`','!','@','#','$','%','^','&','*','(',')','-','_','+','=',};
+            for(int i = 0; i < x.Length; i++)
+            {
+                if (DS.Contains(x[i]))
+                {
+                    return false;
+                }
+                else if(Int32.TryParse(x[i].ToString(),out int y))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private void ngay_nhan_Validating(object sender, CancelEventArgs e)
+        {
+
+            if (DateTime.TryParseExact(ngay_nhan.Text, "d/M/yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out ngaynhan) && ngay_nhan.Text.Length!=0)
+            {
+                e.Cancel = false;
+                errorProvider1.SetError(ngay_nhan,null);
+            }
+            else
+            {
+                e.Cancel = true;
+                errorProvider1.SetError(ngay_nhan, "Vui lòng nhập ngày nhập");
+            }
+        }
+
+        private void nguoi_nhan_Validating(object sender, CancelEventArgs e)
+        {
+            if (!Only_letter(nguoi_nhan.Text)||nguoi_nhan.Text.Length==0)
+            {
+                //e.Cancel = true;
+                errorProvider1.SetError(nguoi_nhan, "Không định dạng được người nhận");
+            }
+            else
+            {
+                e.Cancel = false;
+                errorProvider1.SetError(nguoi_nhan, null);
+            }
+        }
+
+        private void BM3_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (this.DialogResult == DialogResult.Cancel)
+            {
+                AutoValidate = AutoValidate.Disable;
+                this.Close();
+            }
+        }
+
+
+        DateTime ngaynhan;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            if (ValidateChildren(ValidationConstraints.Enabled))
+            {
+                
+                tensach = ten_sach.Text;
+                theloai = the_loai.Text;
+                nguoinhan = nguoi_nhan.Text;
+                tacgia = Tac_gia.Text;
+                namxb = Convert.ToInt32(nam_xb.Text);
+                //ngaynhan = DateTime.ParseExact(ngay_nhan.Text, "d/M/yyyy", CultureInfo.InvariantCulture);
+                MessageBox.Show(ngaynhan.ToString("dd/MM/yyyy"));
+                //DateTime.TryParseExact(ngay_nhan.Text, "d/M/yyyy",CultureInfo.InvariantCulture,DateTimeStyles.None,out ngaynhan);
+            }
+        }
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            if (keyData == Keys.Escape)
+            {
+                //CausesValidation = false;
+                AutoValidate = AutoValidate.Disable;
+                this.Close();
+                return true;
+            }
+            return base.ProcessCmdKey(ref msg, keyData);
+        }
+        
+    }
+}
