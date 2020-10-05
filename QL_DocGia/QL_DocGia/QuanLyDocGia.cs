e2a0816793a9020bd665538be31b0f7470ce50ac
kun09-tker <72250015+kun09-tker@users.noreply.github.com>
2020-10-05 12:13:21 +0700

    Add files via upload
    Them QLDocGia

---
+++ QL_DocGia/QL_DocGia/QuanLyDocGia.cs
@@ -0,0 +1,32 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using BM2;
+
+
+namespace QL_DocGia
+{
+    public partial class QuanLyDocGia : Form
+    {
+        public QuanLyDocGia()
+        {
+            InitializeComponent();
+        }
+
+
+        private void thêmToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            TheDocGia x = new TheDocGia();
+            this.Hide();
+            x.ShowDialog();
+            this.Show();
+        }
+    }
+}
