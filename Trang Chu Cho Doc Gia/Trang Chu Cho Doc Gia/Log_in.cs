f436020eeb8c4e571bdc6b95915b67076f1a9fac
DarkDipper <72306288+DarkDipper@users.noreply.github.com>
2020-10-11 08:47:21 +0700

    Chỉnh sửa tên folder và cập nhật Log_in,Trang chủ cho độc giả

---
+++ Trang Chu Cho Doc Gia/Trang Chu Cho Doc Gia/Log_in.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace  Trang_Chu
+{
+    public partial class Log_in : Form
+    {
+        public Log_in()
+        {
+            InitializeComponent();
+        }
+
+        private void Dang_nhap_button_Click(object sender, EventArgs e)
+        {
+            if (true)
+            {
+                Trang_chu_cho_doc_gia x=new Trang_chu_cho_doc_gia();
+                x.Show();
+                
+            }
+        }
+
+        
+    }
+}
---
+++ Trang Chu Cho Doc Gia/Trang Chu Cho Doc Gia/Trang_chu_cho_doc_gia.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Trang_Chu
+{
+    public partial class Trang_chu_cho_doc_gia : Form
+    {
+        private Button btn;
+        public Trang_chu_cho_doc_gia()
+        {
+            InitializeComponent();
+        }
+        private void Thay_doi_trang_thai_nut(object btnSender)
+        {
+            if (btnSender != null)
+            {
+                if (btn != (Button)btnSender)
+                {
+                    Quay_ve_trang_thai_nut_ban_dau();
+                    btn = (Button)btnSender;
+                    btn.TextImageRelation = TextImageRelation.TextBeforeImage;
+                    btn.BackColor = Color.White;
+                }
+                
+            }
+        }
+        private void Quay_ve_trang_thai_nut_ban_dau()
+        {
+            if (btn != null)
+            {
+                btn.TextImageRelation = TextImageRelation.ImageBeforeText;
+                btn.BackColor = Color.Peru;
+            }
+        }
+        private void button_xem_thong_tin_Click(object sender, EventArgs e)
+        {
+            Thay_doi_trang_thai_nut(button_xem_thong_tin);    
+        }
+
+        private void buttonTra_cuu_sach_Click(object sender, EventArgs e)
+        {
+            Thay_doi_trang_thai_nut(buttonTra_cuu_sach);
+        }
+
+        private void button_dang_xuat_Click(object sender, EventArgs e)
+        {
+            Thay_doi_trang_thai_nut(button_dang_xuat);
+        }
+
+        
+    }
+}
